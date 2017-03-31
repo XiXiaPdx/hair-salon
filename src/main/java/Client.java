4c0eaa0b323e5162c32e7856cc3c3bd6dc0fe70e
Xi Xia <triplextension@gmail.com>
2017-03-31 08:44:40 -0700

    add Clients all, save, getId, and override equals

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,5 @@
 import org.sql2o.*;
+import java.util.List;
 
 public class Client {
   private String c_name;
@@ -12,8 +13,39 @@ public class Client {
   public String getClientName(){
     return c_name;
   }
-  public int getStylistId(){
+  public int getClientStylistId(){
     return client_stylist_id;
   }
+  public int getClientId(){
+    return id;
+  }
+  public void save(){
+    try(Connection con = DB.sql2o.open()) {
+    String sql = "INSERT INTO clients (c_name, client_stylist_id) VALUES (:c_name, :client_stylist_id);";
+    this.id = (int) con.createQuery(sql, true)
+      .addParameter("c_name", this.c_name)
+      .addParameter("client_stylist_id", this.client_stylist_id)
+      .executeUpdate()
+      .getKey();
+  }
+}
+public static List<Client> all (){
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients;";
+      return con.createQuery(sql).executeAndFetch(Client.class);
+    }
+  }
+
+  @Override
+  public boolean equals (Object otherClient){
+    if(!(otherClient instanceof Client)){
+      return false;
+    } else{
+      Client newClient = (Client) otherClient;
+      return this.getClientName().equals(newClient.getClientName()) && this.getClientId() == newClient.getClientId();
+    }
+  }
+
+
 
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -20,7 +20,15 @@ public class ClientTest {
   @Test
   public void getStylistId_1 () {
     Client newClient = new Client("Mary", 1);
-    assertEquals(1, newClient.getStylistId());
+    assertEquals(1, newClient.getClientStylistId());
+  }
+  @Test
+  public void getAllClients_true(){
+    Client newClient1 = new Client("Mary", 1);
+    Client newClient2 = new Client("Bob", 2);
+    newClient1.save();
+    newClient2.save();
+    assertTrue(newClient2.equals(Client.all().get(1)));
   }
 
 
