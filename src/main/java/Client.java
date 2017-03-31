05523f3965936afd9ff26341759ca6163b833060
Xi Xia <triplextension@gmail.com>
2017-03-31 08:31:21 -0700

    add get methods for client name and their stylist id

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,9 +1,19 @@
 import org.sql2o.*;
 
 public class Client {
+  private String c_name;
+  private int client_stylist_id;
+  private int id;
 
-  public Client() {
-
+  public Client(String name, int yourStylistId) {
+    this.c_name = name;
+    client_stylist_id = yourStylistId;
+  }
+  public String getClientName(){
+    return c_name;
+  }
+  public int getStylistId(){
+    return client_stylist_id;
   }
 
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,10 +1,27 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
 
 public class ClientTest {
 
   @Rule
   public DatabaseRule database = new DatabaseRule();
+  @Test
+  public void clientCreated_true () {
+    Client newClient = new Client("Mary", 1);
+    assertTrue(newClient instanceof Client);
+  }
+  @Test
+  public void getClientName_mary () {
+    Client newClient = new Client("Mary", 1);
+    assertEquals("Mary", newClient.getClientName());
+  }
+  @Test
+  public void getStylistId_1 () {
+    Client newClient = new Client("Mary", 1);
+    assertEquals(1, newClient.getStylistId());
+  }
+
 
 }
