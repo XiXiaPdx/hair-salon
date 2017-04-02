a256e676ce187b820ff3875c7c067c6a42a7cefa
Xi Xia <triplextension@gmail.com>
2017-04-02 08:56:19 -0700

    fixed client change name

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -84,6 +84,8 @@ public class App {
     get("/stylists/:stylistId/clients/:clientId", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       Client thisClient = Client.find(Integer.parseInt(request.params(":clientId")));
+      Stylist thisStylist =Stylist.find(Integer.parseInt(request.params(":stylistId")));
+      model.put("stylist", thisStylist);
       model.put("stylists", Stylist.all());
       model.put("client", thisClient);
       model.put("template", "templates/client.vtl");
